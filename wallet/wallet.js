0ead0e6643576354dc1050a4d157299cca836af9
myanzik <myanz.470shrestha@gmail.com>
2023-09-29 09:05:04 +0545

    wallet excercise

---
+++ wallet/wallet.js
@@ -0,0 +1,134 @@
+"use strict";
+
+var path = require("path");
+var fs = require("fs");
+
+var Blockchain = require(path.join(__dirname,"blockchain.js"));
+
+const KEYS_DIR = path.join(__dirname,"keys");
+const PRIV_KEY_TEXT_1 = fs.readFileSync(path.join(KEYS_DIR,"1.priv.pgp.key"),"utf8");
+const PUB_KEY_TEXT_1 = fs.readFileSync(path.join(KEYS_DIR,"1.pub.pgp.key"),"utf8");
+const PRIV_KEY_TEXT_2 = fs.readFileSync(path.join(KEYS_DIR,"2.priv.pgp.key"),"utf8");
+const PUB_KEY_TEXT_2 = fs.readFileSync(path.join(KEYS_DIR,"2.pub.pgp.key"),"utf8");
+
+var wallet = {
+	accounts: {},
+};
+
+addAccount(PRIV_KEY_TEXT_1,PUB_KEY_TEXT_1);
+addAccount(PRIV_KEY_TEXT_2,PUB_KEY_TEXT_2);
+
+// fake an initial balance in account #1
+wallet.accounts[PUB_KEY_TEXT_1].outputs.push(
+	{
+		account: PUB_KEY_TEXT_1,
+		amount: 42,
+	}
+);
+
+main().catch(console.log);
+
+
+// **********************************
+
+async function main() {
+	await spend(
+		/*from=*/wallet.accounts[PUB_KEY_TEXT_1],
+		/*to=*/wallet.accounts[PUB_KEY_TEXT_2],
+		/*amount=*/13
+	);
+
+	await spend(
+		/*from=*/wallet.accounts[PUB_KEY_TEXT_2],
+		/*to=*/wallet.accounts[PUB_KEY_TEXT_1],
+		/*amount=*/5
+	);
+
+	await spend(
+		/*from=*/wallet.accounts[PUB_KEY_TEXT_1],
+		/*to=*/wallet.accounts[PUB_KEY_TEXT_2],
+		/*amount=*/31
+	);
+
+	try {
+		await spend(
+			/*from=*/wallet.accounts[PUB_KEY_TEXT_2],
+			/*to=*/wallet.accounts[PUB_KEY_TEXT_1],
+			/*amount=*/40
+		);
+	}
+	catch (err) {
+		console.log(err);
+	}
+
+	console.log(accountBalance(PUB_KEY_TEXT_1));
+	console.log(accountBalance(PUB_KEY_TEXT_2));
+	console.log(await Blockchain.verifyChain(Blockchain.chain));
+}
+
+function addAccount(privKey,pubKey) {
+	wallet.accounts[pubKey] = {
+		privKey,
+		pubKey,
+		outputs: []
+	};
+}
+
+async function spend(fromAccount,toAccount,amountToSpend) {
+	// TODO
+	var trData = {
+	inputs: [],
+	outputs: [],
+};
+	
+	// pick inputs to use from fromAccount's outputs (i.e. previous txns, see line 22), sorted descending
+	// var sortedInputs =
+	
+// 	for (let input of sortedInputs) {
+// 		// remove input from output-list
+
+
+// 		// do we have enough inputs to cover the spent amount?
+
+	
+	
+// 	}
+	
+	
+	
+// 	if (inputAmounts < amountToSpend) {
+
+// 		throw `Don't have enough to spend ${amountToSpend}!`;
+// 	}	
+
+	// sign and record inputs
+
+	
+	// record output
+
+	
+	// is "change" output needed?
+	
+	
+	// create transaction and add it to blockchain
+	var tr = Blockchain.createTransaction(trData);
+	Blockchain.insertBlock(
+	// TODO .createBlock method
+
+	);	
+	
+	// record outputs in our wallet (if needed)	
+	
+}
+
+function accountBalance(account) {
+	var balance = 0;
+
+// 	if (account in wallet.accounts) {
+
+// 	}	
+	
+	
+// 	return balance;
+	
+}
